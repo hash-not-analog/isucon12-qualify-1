640acc432f1becc23e34d20aeeaf6905729fd45b
anko9801 <daiusa3@icloud.com>
2022-07-23 13:46:00 +0900

    fix PlayerRow initialize cap:0

--- go/tenant_player.go
+++ go/tenant_player.go
@@ -91,7 +91,7 @@ func playersAddHandler(c echo.Context) error {
 
 	pds := make([]PlayerDetail, 0, len(displayNames))
 
-	players := make([]PlayerRow, len(displayNames))
+	players := make([]PlayerRow, 0, len(displayNames))
 	for _, displayName := range displayNames {
 		id, err := dispenseID(ctx)
 		if err != nil {
