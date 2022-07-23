dfd72978f1a5fd0a360971997e90a70d3dba604b
logica <84759675+logica0419@users.noreply.github.com>
2022-07-23 12:35:10 +0900

    cache player

--- go/isuports.go
+++ go/isuports.go
@@ -404,11 +404,15 @@ type PlayerRow struct {
 	UpdatedAt      int64  `db:"updated_at"`
 }
 
+var playerCache = helpisu.NewCache[string, PlayerRow]()
+
 // 参加者を取得する
 func retrievePlayer(ctx context.Context, tenantDB dbOrTx, id string) (*PlayerRow, error) {
-	var p PlayerRow
-	if err := tenantDB.GetContext(ctx, &p, "SELECT * FROM player WHERE id = ?", id); err != nil {
-		return nil, fmt.Errorf("error Select player: id=%s, %w", id, err)
+	p, ok := playerCache.Get(id)
+	if ok {
+		if err := tenantDB.GetContext(ctx, &p, "SELECT * FROM player WHERE id = ?", id); err != nil {
+			return nil, fmt.Errorf("error Select player: id=%s, %w", id, err)
+		}
 	}
 	return &p, nil
 }
