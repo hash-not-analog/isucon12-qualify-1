a848e445d6aeea05097ec1015d0f2109c3b61ecc
anko9801 <daiusa3@icloud.com>
2022-07-23 11:30:19 +0900

    add newcache

--- go/isuports.go
+++ go/isuports.go
@@ -196,6 +196,8 @@ func Run() {
 	adminDB.SetMaxOpenConns(10)
 	defer adminDB.Close()
 
+	tenantDBs = helpisu.NewCache[int, *sqlx.DB]()
+
 	go http.ListenAndServe(":6060", nil)
 
 	port := getEnv("SERVER_APP_PORT", "3000")
